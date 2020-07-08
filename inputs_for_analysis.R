e3c6ba7a6eeb732f90530280426a90da8f9ed160
ppreshant <ppresh.ant@gmail.com>
2020-07-08 15:47:51 -0500

    Corrected BCoV std curve intercept error - Updated the vaccine concentration : 22123 -> 5430.3 - Corrected the changed volumn name of volume measured

--- inputs_for_analysis.R
+++ inputs_for_analysis.R
@@ -14,7 +14,7 @@ title_name <- flnm
 std_par <- tibble(                       # Input the slope and intercept from standard curve of various primer pairs/targets here - Target should match Target field (provided in excel sheet - Sample input reference.csv) 
   target = c('BRSV_N', 'BCoV_M', 'N1_CoV2', 'N2_CoV2', 'N1_multiplex',  'N2_multiplex'),
   slope =  c(-3.61, -3.47, -2.98, -3.12, -3.08, -3.06),
-  intercept = c(38, 39, 39, 40, 38, 37) # values for various targets
+  intercept = c(38, 38, 39, 40, 38, 37) # values for various targets
 )
 template_volume <- 4 # ul template volume in qPCR reaction
 
